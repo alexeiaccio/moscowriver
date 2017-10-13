a0a7e208d9dea8e30785811c8cd28ba08ed13ff2
Alexei Accio <accio@aledo-pro.ru>
2017-10-13 12:37:05 +0300

    tooltips

---
+++ src/components/MapTooltip/MapTooltip.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ToolTip from 'react-portal-tooltip'
+
+class MapTooltip extends React.Component {
+  state = {
+    isTooltipActive: false
+  }
+  showTooltip() {
+    this.setState({isTooltipActive: true})
+  }
+  hideTooltip() {
+    this.setState({isTooltipActive: false})
+  }
+  render() {
+    return (
+      <div className="container mt-5">
+        <p id={`place-${this.props.id}`} onMouseEnter={::this.showTooltip} onMouseLeave={::this.hideTooltip}>{this.props.text}</p>
+        <ToolTip active={this.state.isTooltipActive} position="top" arrow="center" parent={`#place-${this.props.id}`} group="first">
+          <div>
+            <p>{this.props.content}</p>
+          </div>
+        </ToolTip>
+      </div>
+    )
+  }
+}
+
+export default MapTooltip
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Helmet from "react-helmet";
 import GoogleMapBackground from "../components/GoogleMapBackground/GoogleMapBackground";
+import MapTooltip from "../components/MapTooltip/MapTooltip";
 import config from "../../gatsby-site-config";
 
 class AboutPage extends Component {
@@ -9,6 +10,10 @@ class AboutPage extends Component {
       <div className="about-container">
         <Helmet title={`About | ${config.siteTitle}`} />
         <GoogleMapBackground />
+        <div className='row'>
+          <MapTooltip className="col" id='01' text='First' content='First content'/>
+          <MapTooltip className="col" id='02' text='Second' content='Second content'/>
+        </div>        
       </div>
     );
   }

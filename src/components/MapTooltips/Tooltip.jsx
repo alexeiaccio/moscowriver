241e827a17a779182522b99cd8faff23b76a3c87
Alexei Accio <accio@aledo-pro.ru>
2017-10-16 14:43:05 +0300

    fix image

--- src/components/MapTooltips/Tooltip.jsx
+++ src/components/MapTooltips/Tooltip.jsx
@@ -4,9 +4,9 @@ import ToolTip from 'react-portal-tooltip'
 class Tooltip extends React.Component {
   style = {
     style: {
-      padding: 'inherite',
+      padding: 'null',
       borderRadius: 10,
-      boxShadow: '0px 4px 10px rgba(38, 26, 18, .25)'
+      boxShadow: '0px 4px 10px rgba(38, 26, 18, 0.25)',
     },
     arrowStyle: {
       display: 'none'

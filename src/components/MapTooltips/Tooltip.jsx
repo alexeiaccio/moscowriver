9decde5c4c24fd5c5a70fedac40cee671662b17a
Alexei Accio <accio@aledo-pro.ru>
2017-10-16 14:14:55 +0300

    fix tooltips

--- src/components/MapTooltips/Tooltip.jsx
+++ src/components/MapTooltips/Tooltip.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 import ToolTip from 'react-portal-tooltip'
 
 class Tooltip extends React.Component {
+  style = {
+    style: {
+      padding: 'inherite',
+      borderRadius: 10,
+      boxShadow: '0px 4px 10px rgba(38, 26, 18, .25)'
+    },
+    arrowStyle: {
+      display: 'none'
+    }
+  }
+
   state = {
     isTooltipActive: false,
     isQuoteActive: false
@@ -20,10 +31,10 @@ class Tooltip extends React.Component {
     return (
       <div className={this.props.className} key={this.props.id}>
 	      <p id={`sector-${this.props.id}`} onMouseEnter={::this.showTooltip} onClick={::this.showQuote} onMouseLeave={::this.hideTooltip}>&nbsp;</p>
-	      <ToolTip active={this.state.isTooltipActive} position="top" arrow="center" parent={`#sector-${this.props.id}`} group="map">
+	      <ToolTip active={this.state.isTooltipActive} position="top" arrow="center" parent={`#sector-${this.props.id}`} style={this.style} group="map">
           <div>Участок {this.props.sector}. {this.props.embankment}</div>
         </ToolTip>
-        <ToolTip active={this.state.isQuoteActive} position="top" arrow="center" parent={`#sector-${this.props.id}`} group="map">
+        <ToolTip active={this.state.isQuoteActive} position="top" arrow="center" parent={`#sector-${this.props.id}`} style={this.style} group="map">
 	        <div dangerouslySetInnerHTML={{ __html: this.props.html }} />
 	      </ToolTip>
 	    </div>

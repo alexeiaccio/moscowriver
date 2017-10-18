289c1a62d0f12a47ef79627faa8d921d600273d2
Alexei Accio <accio@aledo-pro.ru>
2017-10-18 17:35:56 +0300

    style tooltip

--- src/components/MapTooltips/MapTooltips.jsx
+++ src/components/MapTooltips/MapTooltips.jsx
@@ -8,11 +8,11 @@ class MapTooltips extends React.Component {
 
     this.props.quotesEdges.forEach(quotesEdge => {
       quotesList.push({
-        sector: `<div class="map-embankment"><span class="text-secondary">Участок ${quotesEdge.node.frontmatter.sector/10}.</span> ${quotesEdge.node.frontmatter.embankment}</div>`,
+        sector: `<div class="map-embankment"><span class="text-secondary">Участок ${quotesEdge.node.frontmatter.sector/10}.</span>&nbsp;<span class="text-warning">${quotesEdge.node.frontmatter.embankment}</span></div>`,
         embankment: quotesEdge.node.frontmatter.embankment,
         id: quotesEdge.node.frontmatter.sector,
         question: quotesEdge.node.frontmatter.question,
-        html: `<div class="map-top"><span class="text-secondary">Участок ${quotesEdge.node.frontmatter.sector/10}.</span> ${quotesEdge.node.frontmatter.embankment}</div><div class="map-quote">${quotesEdge.node.html}</div>`,        
+        html: `<div class="map-top"><span class="text-secondary">Участок ${quotesEdge.node.frontmatter.sector/10}.</span>&nbsp;<span class="text-warning">${quotesEdge.node.frontmatter.embankment}</span></div><div class="map-quote">${quotesEdge.node.html}</div>`,        
       });
     });
     return quotesList;

93c3f57fa21a3ccb879f6b12aff40212d74b0169
Alexei Accio <32056666+alexeiaccio@users.noreply.github.com>
2018-05-10 11:52:24 +0300

    prismic rich-text, research description

--- src/components/ResearchPage/ResearchDescription.js
+++ src/components/ResearchPage/ResearchDescription.js
@@ -1,6 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { Fragment } from 'react'
+import styled, { keyframes } from 'styled-components'
 import { key } from 'styled-theme'
+import { RichText } from 'prismic-reactjs'
+
+import SmallWave from '../../assets/SmallWave.svg'
+
+const linkResolver = doc => (
+	doc.type !== 'homepage'
+	? `#${doc.uid}`
+	: '/'
+)
+
+const shiftBack = keyframes`
+  0% {
+    left: -14px;
+  }
+  100% {
+    left: 0px;
+  }
+`
 
 const Description = styled.div`
   width: ${({width}) =>  width}px;
@@ -12,9 +30,39 @@ const Description = styled.div`
   line-height: ${({size}) => key(['lineHeights', size - 1])};
   font-weight: ${key('fontWeights.medium')};
   border: 10px solid ${key('colors.bright.blue')};
+  & a {
+    position: relative;
+    color: ${key('colors.white')};
+    font-weight: ${key('fontWeights.semibold')};
+    text-decoration: none;
+    transition: all .2s ease-in-out;
+    &:hover {
+      color: ${key('colors.bright.blue')};
+    }
+    &::after {
+      content: '';
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 28px;
+      display: block;
+      height: 7px;
+      background: url(${SmallWave}) repeat-x;
+      background-size: 15px 7px;
+      animation: ${shiftBack} .4s linear infinite;
+      animation-play-state: paused;
+    }
+    &:hover::after {
+      animation-play-state: running;
+    }
+  }
 `
-export default ({width, size, data: { description }}) => (
+
+export default ({width, size, data: { description }}) => {
+  const descriptionText = description.map(item => Object.assign({}, item, { type: 'paragraph' }))
+
+  return (
   <Description width={width} size={size} >
-  { description }
+  { RichText.render(descriptionText, linkResolver) }
   </Description>
-)
+)}

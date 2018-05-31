405a3e0e57ef901a02da1696f7daa8cbd9b3c491
Alexei Accio <32056666+alexeiaccio@users.noreply.github.com>
2018-05-31 10:58:37 +0300

    maps city 1 2

--- src/components/ResultPage/maps/MapsMarks-krasnopresnenskaya-naberezhnaya-city-1.js
+++ src/components/ResultPage/maps/MapsMarks-krasnopresnenskaya-naberezhnaya-city-1.js
@@ -3,25 +3,24 @@ import styled from 'styled-components'
 import { key } from 'styled-theme'
 
 const Svg = styled.svg`
-  min-width: 702px;
+  min-width: 650px;
   height: 500px;
   z-index: 1;
   @media (max-width: 1199px) {
     margin-left: -110px;
   }
   transition: all .2s ease-in-out;
-  transform: ${({second}) => second ? 'scale(1.4) translate(95px, 70px)' : 'scale(1.4) translate(-75px, -70px)'};
 `
 
 const Map = styled.div`
-  width: 1400px;
-  height: 700px;
+  width: 650px;
+  height: 500px;
   position: absolute;
-  top: -130px;
-  right: -470px;
-  background: ${({back}) => 'url(' + back + ') no-repeat'};
+  top: 0;
+  right: 0;
+  background: url(${({back}) =>  back}) no-repeat;
+  background-size: cover;
   transition: all .2s ease-in-out;
-  transform: ${({second}) => second ? 'translate(10px, 130px)' : 'translate(-225px, -70px)'};
 `
 
 const MapsMarks = ({state, uid}) => {
@@ -35,62 +34,27 @@ const MapsMarks = ({state, uid}) => {
   return (
     <Fragment>
       <Map back={MapsBack} second={state.items[0][6] || state.items[1][3] ? true : false} />
-      <Svg
-        second={state.items[0][6] || state.items[1][3] ? true : false}
-        viewBox="0 0 702 500" version="1.1" xmlns="http://www.w3.org/2000/svg"
-      >
+      <Svg width="650" height="500" viewBox="0 0 650 500" fill="none" xmlns="http://www.w3.org/2000/svg" >
         <g id="Canvas" fill="none">
-          {state.activeList === 2 &&
-            <g id="maps_2" strokeLinecap="round">
-              <path id="mark_2-7" d="M 81.5 207.5L 75 185.5L 73 181.5L 70.5 172.5L 64.5 143.5L 55.5 122.5L 42 97.5L 36.5 84.5L 21.5 38L 0 0" transform="translate(511.5 242.5)" stroke="#C4549A" strokeWidth={state.items[2][7] ? 10 : 2} />
-              <g id="mark_2-6" transform="translate(583 411)" fill="#F05A5A" stroke="#F05A5A" strokeWidth={state.items[2][6] ? 10 : 0} >
-                <path d="M 6 21.1845L 12.6504 19L 14.1292 23.5018L 7.47875 25.6863L 6 21.1845Z" />
-                <path d="M 0 2.18452L 6.6504 0L 8.12916 4.50181L 1.47875 6.68633L 0 2.18452Z" />
-              </g>
-              <g id="mark_2-5" transform="translate(517 248.5)" fill="#00BDF2" stroke="#00BDF2" strokeWidth={state.items[2][5] ? 10 : 0} >
-                <path d="M 50.0004 118.685L 56.6508 116.501L 58.1296 121.002L 51.4792 123.187L 50.0004 118.685Z" />
-                <path d="M 22.3498 56.185L 29.0002 54.0005L 30.479 58.5023L 23.8286 60.6868L 22.3498 56.185Z" />
-                <path d="M 0 3.5L 6 0L 8.5 4L 2.34984 7.68535L 0 3.5Z"/>
-                <path d="M 35.5 71.5L 39 70.5L 41 76L 37.5 77L 35.5 71.5Z" />
-                <path d="M 40.5 84L 44 83L 46 88.5L 42.5 89.5L 40.5 84Z" />
-              </g>
-              <g id="mark_2-4" transform="translate(518 239)" fill="#F05A5A" stroke="#F05A5A" strokeWidth={state.items[2][4] ? 10 : 0} >
-                <path d="M 28 74.5C 28 75.8807 26.8807 77 25.5 77C 24.1193 77 23 75.8807 23 74.5C 23 73.1193 24.1193 72 25.5 72C 26.8807 72 28 73.1193 28 74.5Z" />
-                <path d="M 68 78.5C 68 79.8807 66.8807 81 65.5 81C 64.1193 81 63 79.8807 63 78.5C 63 77.1193 64.1193 76 65.5 76C 66.8807 76 68 77.1193 68 78.5Z" />
-                <path d="M 35 59C 35 60.3807 33.8807 61.5 32.5 61.5C 31.1193 61.5 30 60.3807 30 59C 30 57.6193 31.1193 56.5 32.5 56.5C 33.8807 56.5 35 57.6193 35 59Z" />
-                <path d="M 65.5 70.5C 65.5 71.8807 64.3807 73 63 73C 61.6193 73 60.5 71.8807 60.5 70.5C 60.5 69.1193 61.6193 68 63 68C 64.3807 68 65.5 69.1193 65.5 70.5Z" />
-                <path d="M 7.5 21C 7.5 22.3807 6.38071 23.5 5 23.5C 3.61929 23.5 2.5 22.3807 2.5 21C 2.5 19.6193 3.61929 18.5 5 18.5C 6.38071 18.5 7.5 19.6193 7.5 21Z" />
-                <path d="M 5 2.5C 5 3.88071 3.88071 5 2.5 5C 1.11929 5 0 3.88071 0 2.5C 0 1.11929 1.11929 0 2.5 0C 3.88071 0 5 1.11929 5 2.5Z" />
-              </g>
-              <path id="mark_2-3" d="M 27 0L 0 9L 33.6957 102L 17.1957 111L 19.5 117.5" transform="translate(590 299)" stroke="#F0C41B" strokeWidth={state.items[2][3] ? 10 : 2} />
-              <path id="mark_2-2" d="M 94.5 190.5L 79 196L 59 135L 46.5 96.5L 73.5 83M 35.5 78L 23 38.5L 0 0" transform="translate(516.5 238.5)" stroke="#3095BF" strokeWidth={state.items[2][2] ? 10 : 2} />
-              <path id="mark_2-1" d="M 79.5 204.5L 63.5 147.5L 50.5 116L 38.5 97L 43 93.5L 50.5 111.5M 35 77.5L 22 38L 0 0" transform="translate(515 241)" stroke="#53B36C" strokeWidth={state.items[2][1] ? 10 : 2} />
-              <path id="mark_2-0" d="M 103.5 56L 76.5 67.5L 106.461 148L 94.461 154L 91.6915 145.5L 67.1915 156L 59 128.5L 49.5 100L 42 81.5L 37 81.5L 22.5 37.5L 0 0" transform="translate(511.5 240.5)" stroke="#EB5A59" strokeWidth={state.items[2][0] ? 10 : 2} />
-              {/* Active */}
-              <use xlinkHref={`#mark_2-${ getActive(2) || 0 }`} />
-            </g>
-          }
           {state.activeList === 1 &&
-            <g id="maps_1" transform="translate(5 0)">
-              <g id="mark_1-4" transform="translate(517 272)" fill="#EB5A59" stroke="#EB5A59" strokeWidth={state.items[1][4] ? 10 : 0} >
-                <path d="M 65.8704 135.717C 65.8704 139.859 62.5149 143.217 58.3756 143.217C 54.2364 143.217 50.8809 139.859 50.8809 135.717C 50.8809 131.575 54.2364 128.217 58.3756 128.217C 62.5149 128.217 65.8704 131.575 65.8704 135.717Z" />
-                <path d="M 14.9896 7.5C 14.9896 11.6421 11.634 15 7.49478 15C 3.35553 15 0 11.6421 0 7.5C 0 3.35786 3.35553 0 7.49478 0C 11.634 0 14.9896 3.35786 14.9896 7.5Z" />
-              </g>
-                <ellipse id="mark_1-3" cx="7.49478" cy="7.5" rx="7.49478" ry="7.5" transform="translate(191.878 46.373)" fill="#EB5A59" stroke="#EB5A59" strokeWidth={state.items[1][3] ? 10 : 0} />
-                <ellipse id="mark_1-2" cx="7.49478" cy="7.5" rx="7.49478" ry="7.5" transform="translate(588 372)" fill="#EB5A59" stroke="#EB5A59" strokeWidth={state.items[1][2] ? 10 : 0} />
-                <ellipse id="mark_1-1" cx="7.49478" cy="7.5" rx="7.49478" ry="7.5" transform="translate(528.863 305.497)" fill="#EB5A59" stroke="#EB5A59" strokeWidth={state.items[1][1] ? 10 : 0} />
-                <ellipse id="mark_1-0" cx="7.49478" cy="7.5" rx="7.49478" ry="7.5" transform="translate(536 228)" fill="#EB5A59" stroke="#EB5A59" strokeWidth={state.items[1][0] ? 10 : 0} />
+            <g id="maps_1" strokeLinecap="round" >
+              <path id="mark_1-0" d="M0.00948114 0C-0.157186 22.8333 1.80948 74.9 11.0095 100.5C19.4606 124.016 29.532 143.212 43.4063 156.5M171.51 168.5C167.899 170.067 162.145 172.341 155.51 174.591M155.51 174.591C145.911 177.847 134.47 181.054 125.01 182C105.01 184 83.0095 180.5 60.0095 168.5C53.9118 165.319 48.4116 161.294 43.4063 156.5M155.51 174.591L128.51 106.5M43.4063 156.5L82.0095 103"
+                transform="translate(116.49 176.5)" stroke="#EB5A59" strokeWidth={state.items[1][0] ? 8 : 2} />
+              <path id="mark_1-1" d="M0 227L21 267.5M136.5 130L162.5 174M181.5 0L239.5 103" transform="translate(289.5 78.5)" stroke="#F0C41B"
+                strokeWidth={state.items[1][1] ? 8 : 2} />
+              <path id="mark_1-2" d="M0 109L180.5 0" transform="translate(396 32)" stroke="#53B36C" strokeWidth={state.items[1][2] ? 8 : 2} />
             </g>
           }
           {state.activeList === 0 &&
             <g id="maps_0">
-              <path id="mark_0-0" d="M 45.6057 119.986C 34.853 104.418 29.6619 85.8838 24.8417 65.867C 19.3692 43.462 9.50106 23.6204 0 3.25376L 6.83464 0C 10.9481 6.74854 17.3117 17.7149 22.0083 30.0069C 26.7049 42.2989 31.3448 54.5909 35.7369 71.5828C 39.3253 85.4655 48.0704 107.013 51.9944 116.051L 45.6057 119.986Z" transform="translate(552.014 339.565)" fill="#945BA4" stroke="#945BA4" strokeWidth={state.items[0][0] ? 10 : 0} />
-              <path id="mark_0-1" d="M 31.8875 78.493C 21.8763 52.5453 14.0898 26.5976 0 3.61529L 5.78044 0C 12.5243 10.0023 26.8068 33.1161 29.9861 45.5527C 33.1653 57.9893 38.2954 77.4878 40.4631 85.6824L 36.3465 89.2977C 34.8065 85.7393 33.3122 82.1429 31.8875 78.493Z" transform="translate(508.631 238.978)" fill="#C4549A" stroke="#C4549A" strokeWidth={state.items[0][1] ? 10 : 0} />
-              <path id="mark_0-6" d="M 2.30324 33.5005L 0 17.3705C 13.271 14.3596 45.3409 7.24084 67.4519 2.85348C 95.0908 -2.63073 124.046 0.595276 158.594 5.75688C 186.233 9.88616 232.846 39.3073 252.698 53.5017L 250.395 57.3729C 237.418 50.4247 231.565 47.4201 215.541 39.6547C 204.717 34.4094 189.511 26.2882 179.323 22.8547C 147.736 12.2089 115.491 11.5637 89.8262 13.1767C 69.2945 14.4671 22.9227 27.2636 2.30324 33.5005Z" transform="matrix(1 9.90373e-05 -9.88996e-05 1 77.0068 40)" fill="#53B36C" stroke="#53B36C" strokeWidth={state.items[0][6] ? 10 : 0} />
-              <path id="mark_0-3" d="M 11.9222 0L 0 5.78447L 4.33533 16.2688L 16.6188 10.1228L 11.9222 0Z" transform="translate(545.12 325.381)" fill="#000000" stroke="#000000" strokeWidth={state.items[0][3] ? 10 : 0} />
-              <path id="mark_0-2" d="M 7.58683 0L 0 3.68102L 4.33533 14.1654L 12.2834 10.1228L 7.58683 0Z" transform="translate(580.877 427.373)" fill="#000000" stroke="#000000" strokeWidth={state.items[0][2] ? 10 : 0} />
-              <path id="mark_0-4" d="M 13.5933 0L 0 9.04179L 8.15911 22.6443L 21.7655 15.1843L 13.5933 0Z" transform="matrix(0.877177 -0.480168 0.479654 0.877458 449.877 186.824)" fill="#F0C41B" stroke="#F0C41B" strokeWidth={state.items[0][4] ? 10 : 0} />
-              <path id="mark_0-5" d="M 9.45455 0L 0 4.2L 4.36364 14L 13.8182 10.85L 16 4.2L 9.45455 0Z" transform="translate(567.877 383.373)" fill="#F0C41B" stroke="#F0C41B" strokeWidth={state.items[0][5] ? 10 : 0} />
+              <path id="mark_0-0" d="M26.5 308.5L15.5 310L11 271V262L0 186L12.5 184.5C13.5 188.833 16.2 199.8 19 209C22.5 220.5 44.5 246 64.5 256C84.5 266 109 268 123 266.5C137 265 173.5 249.5 188.5 242.5C203.5 235.5 266.5 201.5 280.5 191.5C291.7 183.5 325.833 156.167 341.5 143.5L507 0L514.5 10L497 28L405.5 105V109L363 147H359.5L328.5 172V179.5L314 187L286.5 209.5L223.5 246L190.5 262L188.5 258.5L187 260.5V268L168.5 275.5L165 269.5L80 299L26.5 306V308.5Z"
+                transform="translate(119 88.5)" fill="#F0C41B" stroke="#F0C41B" strokeWidth={state.items[0][0] ? 10 : 0} />
+              <path id="mark_0-1" d="M9.60053 93L167.6 0L179.601 19.5L187.601 14.5L192.601 24.5L185.601 29L195.101 46L51.6006 130L53.6006 134L37.1006 142.5L35.6005 139.5C24.6005 141 13.1005 142.5 3.60048 126C-4.48216 111.962 2.60049 99 9.60053 93Z"
+                transform="translate(198.9 129)" fill="#EB5A59" stroke="#EB5A59" strokeWidth={state.items[0][1] ? 10 : 0} />
+              <rect id="mark_0-2" width="34.2562" height="11.9267" transform="translate(346 328.355) rotate(-28.5171)" fill="#979797" stroke="#979797" strokeWidth={state.items[0][2] ? 10 : 0} />
+              <path id="mark_0-3" d="M12 24C5.37258 24 0 18.6274 0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 14.52 23.2232 16.8586 21.8961 18.7893L73.2202 101.131C73.8009 101.045 74.3952 101 75 101C81.6274 101 87 106.373 87 113C87 119.627 81.6274 125 75 125C68.3726 125 63 119.627 63 113C63 110.388 63.8348 107.97 65.2521 106L14.1202 23.8133C13.432 23.936 12.7235 24 12 24Z"
+                transform="translate(410 228)" fill="#5D5957" stroke="#5D5957" strokeWidth={state.items[0][3] ? 10 : 0} />
+              <path id="mark_0-4" d="M0 28.5L11.5 21L45.5 0L67 34.5L51.5 47L24.5 64L0 28.5Z" transform="translate(354 229)" fill="#53B36C" stroke="#53B36C" strokeWidth={state.items[0][4] ? 10 : 0} />
               {/* Active */}
               <use xlinkHref={`#mark_0-${ getActive(0) || 0 }`} />
             </g>
--- src/components/ResultPage/maps/MapsMarks-krasnopresnenskaya-naberezhnaya-city-2.js
+++ src/components/ResultPage/maps/MapsMarks-krasnopresnenskaya-naberezhnaya-city-2.js
@@ -2,35 +2,29 @@ import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
 import { key } from 'styled-theme'
 
-
 const Svg = styled.svg`
-  min-width: 702px;
+  min-width: 650px;
   height: 500px;
   z-index: 1;
-  transition: all .2s ease-in-out;
-  transform: ${({second}) => second ? 'scale(1.4) translate(100px, 75px)' : 'scale(1.4) translate(-80px, -70px)'};
   @media (max-width: 1199px) {
-    margin-left: -100px;
-    transform: ${({second}) => second ? 'scale(1.4) translate(150px, 75px)' : 'scale(1.4) translate(-80px, -70px)'};
+    margin-left: -110px;
   }
+  transform: scale(1.5) translate(20px, -70px);
 `
 
 const Map = styled.div`
-  width: 1400px;
-  height: 700px;
+  width: 975px;
+  height: 750px;
   position: absolute;
-  top: -130px;
-  right: -470px;
-  background: ${({back}) => 'url(' + back + ') no-repeat'};
-  transition: all .2s ease-in-out;
-  transform: ${({second}) => second ? 'translate(25px, 130px)' : 'translate(-230px, -70px)'};
-  @media (max-width: 1199px) {
-    transform: ${({second}) => second ? 'translate(90px, 130px)' : 'translate(-230px, -70px)'};
-  }
+  top: 0;
+  right: 0;
+  background: url(${({back}) =>  back}) no-repeat;
+  transform: translate(193px,-228px);
 `
 
 const MapsMarks = ({state, uid}) => {
   const MapsBack = require(`./maps-${uid}.png`)
+  const MapsBackBack = require(`./maps-${uid}-back.png`)
   const getActive = (active) =>
   state.items[active]
     .map((item, i) => item && i)
@@ -38,23 +32,55 @@ const MapsMarks = ({state, uid}) => {
     .join()
   return (
     <Fragment>
-      <Map back={MapsBack} second={state.items[0][3] ? true : false} />
-      <Svg
-        second={state.items[0][3] ? true : false}
-        viewBox="0 0 702 500" version="1.1" xmlns="http://www.w3.org/2000/svg"
-      >
+      <Map back={MapsBack} />
+      <Svg width="650" height="500" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <pattern id="img1" patternUnits="userSpaceOnUse" width="100%" height="100%">
+            <image xlinkHref={MapsBackBack} x="0" y="0" width="650" height="500" />
+          </pattern>
+        </defs>
         <g id="Canvas" fill="none">
           {state.activeList === 0 &&
-            <g id="maps_0">
-              <path id="mark_2-0-0" d="M8 0L0 3V7L4 14.5L8 28V36L11.5 41L22.5 36L8 0Z" transform="translate(584 418.5)" fill="#F05A5A" stroke="#F05A5A" strokeWidth={state.items[0][0] ? 10 : 0}/>
-              <path id="mark_2-0-1" d="M5.5 11L0 2.5L5.5 0L7.5 2.5L12 4L43 85.5L35 88.5L31.5 80.5L27 54.5L23.5 49.5L17 31L10.5 23L5.5 11Z"
-                transform="translate(549 333)" fill="#EFC319" stroke="#EFC319" strokeWidth={state.items[0][1] ? 10 : 0}/>
-              <path id="mark_2-0-2" d="M37.5 93.5L42.5 91.5C39.5 76.5 36 59.5 29.5 43.5C26.1034 35.1392 13.6667 10.6667 6.5 0L0 3.5C3.33333 9.16667 10.7 21.6 13.5 26C17 31.5 23.5 46 27 56C29.8 64 35.1667 84.3333 37.5 93.5Z"
-                transform="translate(509.5 237)" fill="#C4549A" stroke="#C4549A" strokeWidth={state.items[0][2] ? 10 : 0}/>
-              <path id="mark_2-0-3" d="M439 200.333L432.5 203.833C428.833 199.333 421.76 187.998 417 183.833C413 180.333 394.167 167 385 161.833C369.833 153.833 337 135.933 327 128.333C320 122.833 317 115.833 307 102.833C299 92.433 278.667 79.1663 269.5 73.833L267 76.333L255 68.333V64.833L250.5 60.833L249 56.833L242 55.833L225.5 47.833C211.167 40.833 179.7 25.633 168.5 20.833C154.5 14.833 120.5 11.833 110.5 11.833C102.5 11.833 88.1667 14.1663 82 15.333C72.8 16.933 23 29.833 17.5 31.833C12 33.833 3 34.333 3 34.333L0 17.333C24.5 12.9998 78.5 3.63333 98.5 0.833333C123.5 -2.66667 148.5 5.83333 164 8.33333C176.4 10.3333 204.5 26.8333 217 34.8333L218.5 39.3333C231.833 45.8333 260.2 59.6333 267 62.8333C273.8 66.0333 299.167 84.8333 311 93.8333L342 121.833C360.333 135.833 399.8 165.633 411 172.833C422.2 180.033 434.333 194.167 439 200.333Z"
-                transform="translate(77 36.667)" fill="#53B36C" stroke="#53B36C" strokeWidth={state.items[0][3] ? 10 : 0}/>
-              {/* Active */}
-              <use xlinkHref={`#mark_2-0-${ getActive(0) || 0 }`} />
+            <g id="maps_1">
+              {/* back */}
+              <rect fill="url(#img1)" width="100%" height="100%"/>
+              {/* back */}
+              <g id="mark_1-0">
+                <path id="Vector" d="M44.6784 64.3523L49.4269 76.2881L50.4341 76.0723L51.945 75.7847L49.5708 61.62C47.9879 52.5604 49.9305 43.1412 55.0386 35.4477L78.8527 1.36615L77.126 -8.7771e-06L64.7513 16.897L51.1536 35.4477C45.0382 42.7817 36.3327 47.5272 26.8358 48.6777L-5.48904e-07 52.7761L0.503622 55.796L22.9508 52.3446C32.1598 50.9785 41.0811 55.8679 44.6784 64.3523Z"
+                  transform="translate(113 308)" fill="#C4549A" stroke="#C4549A" strokeWidth={state.items[0][0] ? 10 : 0} />
+                <circle id="Ellipse" cx="12.5" cy="12.5" r="12.5" transform="translate(136 320)"  fill={state.items[0][0] ? '#F05A5A' : '#000'} stroke="#F05A5A" strokeWidth={state.items[0][0] ? 10 : 0} />
+                <text id="1" transform="translate(136 320)" fill="white" xmlSpace="preserve" fontFamily="Montserrat"
+                  fontStyle="Medium" fontSize="12" fontWeight="500" letterSpacing="0em">
+                  <tspan x="10.2793" y="16.802">1</tspan>
+                </text>
+              </g>
+              <g id="mark_1-1">
+                <path id="Vector_2" d="M-5.48904e-07 18.0474L3.88508 17.616C4.10092 16.0341 5.10816 14.5961 6.61903 13.8771C8.99324 12.7266 11.8711 13.8052 12.9503 16.1779L14.7489 15.8904L50.7938 9.92246L52.5205 9.56295C52.3766 7.47779 53.5997 5.53647 55.5423 4.74555C57.9884 3.73892 60.8663 4.96122 61.8016 7.40589L63.5283 7.04638L64.8233 6.7588L91.0836 -8.7771e-06L91.6591 2.15705L65.3989 9.13156L65.4708 9.49107L49.8585 13.0143L49.6427 12.2953L37.3399 14.5242L13.31 17.8317L13.0222 19.1978C12.8783 19.7731 12.3747 20.2045 11.7991 20.2764L0.575566 21.7863L-5.48904e-07 18.0474Z"
+                  transform="translate(136 378)" fill="#EB5A59" stroke="#EB5A59" strokeWidth={state.items[0][1] ? 10 : 0} />
+                <circle id="Ellipse_2" cx="12.5" cy="12.5" r="12.5" transform="translate(182 395)"  fill={state.items[0][1] ? '#F05A5A' : '#000'} stroke="#F05A5A" strokeWidth={state.items[0][1] ? 10 : 0} />
+                <text id="2" transform="translate(182 395)" fill="white" xmlSpace="preserve" fontFamily="Montserrat"
+                  fontStyle="Medium" fontSize="12" fontWeight="500" letterSpacing="0em">
+                  <tspan x="9.05469" y="16.802">2</tspan>
+                </text>
+              </g>
+              <g id="mark_1-2">
+                <path id="Vector_3" d="M39.2763 0H4.39054e-06V24.019H39.2763V0Z" transform="matrix(0.861835 -0.50717 0.50763 0.861564 331 309.92)"
+                  fill="#979797" stroke="#979797" strokeWidth={state.items[0][2] ? 10 : 0} />
+                <circle id="Ellipse_3" cx="12.5" cy="12.5" r="12.5" transform="translate(325 275)"  fill={state.items[0][2] ? '#F05A5A' : '#000'} stroke="#F05A5A" strokeWidth={state.items[0][2] ? 10 : 0} />
+                <text id="3" transform="translate(325 275)" fill="white" xmlSpace="preserve" fontFamily="Montserrat"
+                  fontStyle="Medium" fontSize="12" fontWeight="500" letterSpacing="0em">
+                  <tspan x="9.06641" y="16.802">3</tspan>
+                </text>
+              </g>
+              <g id="mark_1-3">
+                <path id="Vector_4" d="M21.6523 0H8.78109e-06V4.60245H21.6523V0Z" transform="matrix(0.862135 -0.50667 0.50713 0.861865 379 307.971)"
+                  fill="#EB5A59" stroke="#EB5A59" strokeWidth={state.items[0][3] ? 10 : 0} />
+                <circle id="Ellipse_4" cx="12.5" cy="12.5" r="12.5" transform="translate(393 305)"  fill={state.items[0][3] ? '#F05A5A' : '#000'} stroke="#F05A5A" strokeWidth={state.items[0][3] ? 10 : 0} />
+                <text id="4" transform="translate(393 305)" fill="white" xmlSpace="preserve" fontFamily="Montserrat"
+                  fontStyle="Medium" fontSize="12" fontWeight="500" letterSpacing="0em">
+                  <tspan x="8.48633" y="16.802">4</tspan>
+                </text>
+              </g>
             </g>
           }
         </g>

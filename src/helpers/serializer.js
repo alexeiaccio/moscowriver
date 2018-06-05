ae444725edd7ed2a5c4b9f28f2f67739fd2a8640
Alexei Accio <alexei.accio@gmail.com>
2018-06-05 15:03:43 +0300

    safe serializer

--- src/helpers/serializer.js
+++ src/helpers/serializer.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { RichText } from 'prismic-reactjs'
 const Elements = RichText.Elements
 
-const newLine = text =>
-  text.search(/\n/) > 0
-  ? text.split('\n').map((item, key) => {
-      return <Fragment key={s4()}>{item}<br/></Fragment>
-    })
-  : text
-
+const newLine = text => {
+  if (text !== undefined) {
+    return text.search(/\n/) > 0
+    ? text.split('\n').map((item, key) => {
+        return <Fragment key={s4()}>{item}<br/></Fragment>
+      })
+    : text
+  }
+  return text
+}
 const propsWithUniqueKey = function(props, key) {
   return Object.assign(props || {}, { key })
 }
